00a4721d31fab1260d0b7422202bafa9e92cb5f6
shailendra kumar <shailedrakumar25@gmail.com>
2025-09-28 01:03:07 +0530

    merge dictt

---
+++ Merged_multiple_dictionary.py
@@ -0,0 +1,6 @@
+dict1={"a":1,"b":3,"c":5,"d":5}
+dict2={"name":"shailu","age":19,"student":"btech","branch":"CS"}
+dict3={"e":3,"V":5}
+dict4={"name":"vidhi","age":18,"adress":"iglas"}
+merged_dict={**dict1,**dict2,**dict3,**dict4}
+print(merged_dict)
