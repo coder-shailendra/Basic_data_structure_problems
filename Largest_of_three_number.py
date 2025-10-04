89f1cf69d924572bdae7660e3403732464482157
shailendra kumar <shailedrakumar25@gmail.com>
2025-10-04 10:22:02 +0530

    largest iof three number

---
+++ Largest_of_three_number.py
@@ -0,0 +1,10 @@
+num1=int(input("enter the first num:"))
+num2=int(input("enter the second num:"))
+num3=int(input("enter the third num:"))
+if num1>num2 and num1>num3:
+    print("first number is largest")
+elif num2>num1 and num2>num3:
+    print("second number is largest")
+else:
+    print("third number is largest")
+
