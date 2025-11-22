6848bb15507107f5d22be74ffda5274d8e68298a
shailendra kumar <shailedrakumar25@gmail.com>
2025-11-22 20:36:42 +0530

    solved elligibility ofthe student

---
+++ Student_elligiblitity.py
@@ -0,0 +1,9 @@
+age=float(input("enter the age of the student"))
+
+if age<16.5:
+    print("student_are_not_elligible")
+elif age>16.5 and age<19.5:
+    print("student_are_elligible")
+
+else:
+    print("student_are_not_elligible")
