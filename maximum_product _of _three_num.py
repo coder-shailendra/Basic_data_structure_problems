2299d461447ceae7df88dc10a1e4d2cfbd15c0e7
shailendra kumar <shailedrakumar25@gmail.com>
2025-12-21 10:36:51 +0530

    leetcode problem

---
+++ maximum_product _of _three_num.py
@@ -0,0 +1,14 @@
+def maximumProductofthree(nums):
+    nums.sort()
+    
+    product1 = nums[-1] * nums[-2] * nums[-3]
+    
+    product2 = nums[0] * nums[1] * nums[-1]
+    
+    return max(product1, product2)
+
+nums = [1, 2, 3]
+
+result = maximumProductofthree(nums)
+
+print("Maximum Product:", result)
