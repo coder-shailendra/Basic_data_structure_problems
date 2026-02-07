f52a293f3399195e9b6fb98d0b58485788d7592a
shailendra kumar <shailedrakumar25@gmail.com>
2026-02-07 21:40:02 +0530

    PALINDROME PROBLEM

---
+++ palindrome number.py
@@ -0,0 +1,17 @@
+def palindrome(x):
+    if x < 0:
+        return False
+
+    original = x
+    reverse = 0
+
+    while x > 0:
+        digit = x % 10
+        reverse = reverse * 10 + digit
+        x = x // 10
+
+    return original == reverse
+
+print(palindrome(121))
+print(palindrome(45))
+print(-121)
