91b559be21fabf854181fc8c5e248e6c8dac1962
shailendra kumar <shailedrakumar25@gmail.com>
2026-02-23 09:55:20 +0530

    leetcode problem

---
+++ sum oF two.py
@@ -0,0 +1,7 @@
+def sumoftwo(nums,target):
+    for i in range (len(nums)):
+        for j in range (i+1 , len(nums)):
+            if nums[i]+nums[j]==target:
+                return [i,j]
+print(sumoftwo([2,7,11,15],9))
+print(sumoftwo([3,2,4],6))
